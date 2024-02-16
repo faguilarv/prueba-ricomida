3d1e3296a8fcbb50e4a1031830fdd7026d0f645c
faguilar <franciscoaguilarvergara@gmail.com>
2024-02-15 21:42:31 -0300

    update al ricomida

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,18 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function () {
+    $(enviarCorreo).click(function () {
+        alert("El correo fue enviado correctamente")
+    });
+    $("h3").dblclick(function () {
+        $(this).css({
+            "color": "red",
+        });
+
+    })
+
+
+})
 
 
